86936fbccffba044db251c9b7ebbbbb0e4bcb766
Paulina Kiendys <84099095+paulinakiendys@users.noreply.github.com>
2022-03-15 19:20:16 +0100

    Removed code that will not be used

--- public/assets/js/kill-the-virus.js
+++ public/assets/js/kill-the-virus.js
@@ -3,47 +3,8 @@ const socket = io();
 const startEl = document.querySelector('#start');
 const gameWrapperEl = document.querySelector('#game-wrapper');
 const usernameForm = document.querySelector('#username-form');
-const messagesEl = document.querySelector('#messages'); // ul element containing all messages
-const messageForm = document.querySelector('#message-form');
-const messageEl = document.querySelector('#message');
 
 let username = null;
-
-const addMessageToChat = (message, ownMsg = false) => {
-	// create new `li` element
-	const liEl = document.createElement('li');
-	// set class of `li` to `message`
-	liEl.classList.add('message');
-	// set content of `li` element
-	liEl.innerText = message;
-	if (ownMsg) {
-		liEl.classList.add('you');
-	}
-	// append `li` element to `#messages`
-	messagesEl.appendChild(liEl);
-	// scroll `li` element into view
-	liEl.scrollIntoView();
-}
-const addNoticeToChat = notice => {
-	const liEl = document.createElement('li');
-	liEl.classList.add('notice');
-	liEl.innerText = notice;
-	messagesEl.appendChild(liEl);
-	liEl.scrollIntoView();
-}
-// listen for when a new user connects
-socket.on('user:connected', () => {
-	addNoticeToChat("Someone connected");
-});
-// listen for when a user disconnects
-socket.on('user:disconnected', () => {
-	addNoticeToChat("Someone disconnected");
-});
-// listen for incoming messages
-socket.on('chat:message', message => {
-	console.log("Someone said something:", message);
-	addMessageToChat(message);
-});
 
 // get username from form and show chat
 usernameForm.addEventListener('submit', e => {
@@ -54,26 +15,7 @@ usernameForm.addEventListener('submit', e => {
 	// hide start view
 	startEl.classList.add('hide');
 
-	// show chat view
+	// show game view
 	gameWrapperEl.classList.remove('hide');
 
-	// focus on inputMessage
-	messageEl.focus();
-});
-
-// send message to server
-messageForm.addEventListener('submit', e => {
-	e.preventDefault();
-
-	console.log("Someone submitted something:", messageEl.value);
-	if (!messageEl.value) {
-		return;
-	}
-	// send message to server
-	socket.emit('chat:message', messageEl.value);
-	// add message to chat
-	addMessageToChat(messageEl.value, true);
-	// clear message input element and focus
-	messageEl.value = '';
-	messageEl.focus();
 });
