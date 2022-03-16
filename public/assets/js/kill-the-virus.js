3a3c090e8e222450ea97329275d71c354445b903
glafver <glafver22@gmail.com>
2022-03-16 11:18:31 +0100

    Created function to create a room when users connected

--- public/assets/js/kill-the-virus.js
+++ public/assets/js/kill-the-virus.js
@@ -3,19 +3,39 @@ const socket = io();
 const startEl = document.querySelector('#start');
 const gameWrapperEl = document.querySelector('#game-wrapper');
 const usernameForm = document.querySelector('#username-form');
+const button = document.querySelector('.btn-primary');
+const waiting = document.querySelector('#waiting')
 
 let username = null;
 
+
 // get username from form and show chat
 usernameForm.addEventListener('submit', e => {
-	e.preventDefault();
+    e.preventDefault();
 
-	username = usernameForm.username.value;
+    username = usernameForm.username.value;
 
-	// hide start view
-	startEl.classList.add('hide');
+    socket.emit('user:joined', username, (status) => {
+        // we've received acknowledgement from the server
+        console.log("Server acknowledged that user joined", status);
 
-	// show game view
-	gameWrapperEl.classList.remove('hide');
+        // hiding button 'Play' and showing text that user needs to wait for another user
+        button.classList.add('hide')
+        waiting.classList.remove('hide')
+
+        // if it is the second user we hiding the start screen
+        if (!status.waiting) {
+            startEl.classList.add('hide');
+            gameWrapperEl.classList.remove('hide');
+
+            // if it is the first user we 'listening' for the second user and only when we get the answer hiding start screen
+        } else {
+            socket.on('user:ready', () => {
+                startEl.classList.add('hide');
+                gameWrapperEl.classList.remove('hide');
+            })
+        }
+
+    });
 
 });
